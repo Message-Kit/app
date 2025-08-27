8d0a573404fe4140920197153cb638b1ca80e479
Turbobot <turbobot@vercel.com>
2025-08-28 02:00:15 +0530

    feat(create-turbo): apply official-starter transform

--- src/components/template-cards/action-rows/dropdown-menu.tsx
+++
@@ -1,7 +0,0 @@
-export default function DropdownMenu() {
-    return (
-        <div className="bg-card rounded-xl border p-6 w-full">
-            Action Row Dropdown Menu
-        </div>
-    );
-}
